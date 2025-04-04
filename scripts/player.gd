8b8ced682b9f7aca6decc5da4b8cca142dec56e6
RE-RuLuM <67955488+RE-RuLuM@users.noreply.github.com>
2025-04-04 00:08:40 -0500

    agregando, mobs, seguimiento a jugador y arma

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,3 +5,8 @@ func _physics_process(delta: float) -> void:
 	velocity = direction * 600
 	move_and_slide()
 	
+	if velocity.length() > 0.0:
+		$HappyBoo.play_walk_animation()
+	else:
+		$HappyBoo.play_idle_animation()
+	
