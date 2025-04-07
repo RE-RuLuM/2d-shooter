82716821acaf3b79343405bab33ad4ba0978ddbd
Edgar Moreano <edgar12moreano@hotmail.com>
2025-04-06 22:27:59 -0500

    agregando arma y proyectil junto con animaciones de daño en mobs

--- scripts/gun.gd
+++ scripts/gun.gd
@@ -5,3 +5,14 @@ func _physics_process(delta):
 	if enemies_in_range.size() > 0:
 		var target_enemy: Node2D = enemies_in_range.front()
 		look_at(target_enemy.global_position)
+
+func shoot():
+	const BULLET = preload("res://scenes/bullet.tscn")
+	var new_bullet: Area2D = BULLET.instantiate()
+	new_bullet.global_position = %ShootingPoint.global_position
+	new_bullet.global_rotation = %ShootingPoint.global_rotation
+	%ShootingPoint.add_child(new_bullet)
+
+
+func _on_timer_timeout() -> void:
+	shoot()
